1fd9bff71a795e32792ceaf9d89b2576ec0f40a9
Jtharmon <jtharmon10@gmail.com>
2023-02-22 16:54:00 -0500

    deleted redux file to test Netlify build

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import moviesReducer from "./reducers/movies";
-import UserReducer from "./reducers/user/user";
-
-export const store = configureStore({
-    reducer: { movies: moviesReducer, user: userReducer }
-});
-
