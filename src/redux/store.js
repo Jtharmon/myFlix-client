6b5b0ec2fdab474a7d1803afbac531f97e4ebeb6
Jtharmon <jtharmon10@gmail.com>
2023-02-22 15:52:39 -0500

    took out export from user.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./reducers/movies";
-import {userReducer} from "./reducers/user/user";
+import UserReducer from "./reducers/user/user";
 
 export const store = configureStore({
     reducer: { movies: moviesReducer, user: userReducer }
