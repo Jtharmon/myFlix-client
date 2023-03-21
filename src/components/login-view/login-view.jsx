16f3876c2ce9975d9353adea0b5783b99f05bf9e
Jtharmon <jtharmon10@gmail.com>
2023-03-21 13:27:17 -0400

    nesting fix

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -47,12 +47,9 @@ export const LoginView = ({ onLoggedIn }) => {
     return (
         <Container>
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
